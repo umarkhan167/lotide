61a48e99d41523a717a65328378155f9596b7f80
Umar Khan <umarkhan167@outlook.com>
2021-10-24 18:53:32 -0400

    completed eqArrays function with assert equal function's help

---
+++ eqArrays.js
@@ -0,0 +1,39 @@
+
+//ASSERT EQUAL FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed:   ${actual}   ===  ${expected}`);
+  } else {
+    console.log(`Assertion Failed:   ${actual}   !==  ${expected}`);
+  }
+};
+
+// TEST CODE
+//assertEqual("LHL", "LHL");
+//assertEqual(4, 64);
+
+
+//eqArrays FUNCTION
+
+const eqArrays = function(array1, array2) {
+  if (array1 == null || array2 == null) return false;   
+  if (array1.length !== array2.length) return false;
+
+  for (let i = 0; i<array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false
+  }
+  } return true
+}
+
+
+
+//TEST RUN
+//console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+//console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+//console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+//console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => should PASS
