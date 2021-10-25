912415f4297d51fc07a299d713bb9a2ded39bfed
Umar Khan <umarkhan167@outlook.com>
2021-10-24 23:15:43 -0400

    function that flattens down arrays

---
+++ flatten.js
@@ -0,0 +1,47 @@
+//eqArrays FUNCTION
+
+const eqArrays = function(array1, array2) {
+  if (array1 === null || array2 === null) return false;
+  if (array1.length !== array2.length) return false;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+//TEST RUN
+//console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+//console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+
+//assertArraysEqual FUNCTION
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`Assertion Passed:   ${arr1}   ===  ${arr2}`);
+  } else {
+    console.log(`Assertion Failed:   ${arr1}   !==  ${arr2}`);
+  }
+};
+
+//TEST RUN
+//assertArraysEqual([1, 2, 3], [1, 2, 3]);
+
+
+//flatten FUNCTION
+
+const flatten = function(arr) {
+  let flattened = [];
+  for(let i = 0; i < arr.length; i++) {
+    if(Array.isArray === false) {
+      flattened.push(arr[i]);
+    } else {
+      flattened = flattened.concat(arr[i]);
+    }
+  }
+  return flattened;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
