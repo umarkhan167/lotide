f14005df7f1bfb949e5c52abeef43cd963b4c774
Umar Khan <umarkhan167@outlook.com>
2021-11-10 13:54:40 -0500

    mocha and chai done to headTest file

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,19 @@
-const assertEqual = require('../assertEqual')
-const head = require('../head.js')
+//const assertEqual = require('../assertEqual') - no longer needed
+const assert = require('chai').assert; // Used assertion function from Chai's assertion library, so no need for assertEqual anymore. Works well when paired with Mocha.
+const head = require('../head');
 
-
-// TEST CODE
-assertEqual(head([4,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
-console.log(head([5,6,7]));
+//Running tests using Mocha. Mocha gives us the describe and it functions. Each it is a test, and each test should have at least one assertion.
+describe("#head", () => { 
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns 4 for [4,6,7]", () => {
+    assert.strictEqual(head([4,6,7]), 4);
+  });
+  it (`returns 'Hello' for ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  })
+});
