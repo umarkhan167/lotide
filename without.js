539830f5614190875d308a0406e7ed791d31dfa9
Umar Khan <umarkhan167@outlook.com>
2021-11-21 17:43:30 -0500

    removes unwanted items from array

--- without.js
+++ without.js
@@ -2,13 +2,15 @@
 
 const without = function(source, itemsToRemove) {
   for (let x = 0; x<source.length; x++) {
-    for (let y = 0; y<itemsToRemove; y++) {
-      if (itemsToRemove[y] === source[x]) {
+    for (let y = 0; y<itemsToRemove.length; y++) {
+      if (source[x] === itemsToRemove[y]) {
         source.splice(x, 1)
       }
     }
+    
   }
-  return source
+  return source;
 }
 
 module.exports = without;
+
