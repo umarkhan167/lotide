2db9b7322425803f7e58784299b1c1c043073f69
Umar Khan <umarkhan167@outlook.com>
2021-11-21 17:42:27 -0500

    function now passes all test cases

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -3,7 +3,11 @@ const without = require('../without');
 
 
 //TEST CASES
+assertArraysEqual((without([1, 2, 3], [1])), [2, 3]); // => [2, 3]
+assertArraysEqual((without(["1", "2", "3"], [1, 2, "3"])),["1", "2"]); // => ["1", "2"]
+assertArraysEqual(without([1, 2, 3], [1, 2]), [3]); // => [3]
+
 const words = ["hello", "world", "lighthouse"];
-console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
+without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+assertArraysEqual(words, ["hello", "world"]);
